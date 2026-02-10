77762b3f8f0eb34957990de8f5c1789fbf80e665
lxdua <2753792469@QQ.COM>
2026-02-10 22:39:13 +0800

    修一下换皮肤方式

--- mc_human_v1/mc_human_v1_tool.gd
+++ mc_human_v1/mc_human_v1_tool.gd
@@ -3,14 +3,16 @@ extends BetterMarker3D
 
 const SKIN_TEMPLATE = preload("uid://dtvwc0h2qym6t")
 
+@export_subgroup("Skin")
 @export var skin_texture: Texture2D:
 	set(tex):
 		skin_texture = tex
-		var mesh_root := get_node("%MeshRoot")
-		if mesh_root != null:
-			var material: StandardMaterial3D = null
-			if skin_texture != null:
-				material = SKIN_TEMPLATE.duplicate()
-				material.albedo_texture = skin_texture
-			for mesh_ins: MeshInstance3D in mesh_root.get_children():
-				mesh_ins.set_surface_override_material(0, material)
+		var material: StandardMaterial3D = null
+		if skin_texture != null:
+			material = SKIN_TEMPLATE.duplicate()
+			material.albedo_texture = skin_texture
+		for mesh_ins: MeshInstance3D in skin_mesh_list:
+			mesh_ins.set_surface_override_material(0, material)
+
+@export_subgroup("Mesh")
+@export var skin_mesh_list: Array[MeshInstance3D]
