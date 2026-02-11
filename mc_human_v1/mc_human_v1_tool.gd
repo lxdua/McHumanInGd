f43e6b808e6af68b40bfb346d5316c686ddac896
lxdua <2753792469@qq.com>
2026-02-11 10:20:20 +0800

    改Toon

--- mc_human_v1/mc_human_v1_tool.gd
+++ mc_human_v1/mc_human_v1_tool.gd
@@ -7,12 +7,13 @@ const SKIN_TEMPLATE = preload("uid://dtvwc0h2qym6t")
 @export var skin_texture: Texture2D:
 	set(tex):
 		skin_texture = tex
-		var material: StandardMaterial3D = null
+		skin_material = SKIN_TEMPLATE.duplicate()
 		if skin_texture != null:
-			material = SKIN_TEMPLATE.duplicate()
-			material.albedo_texture = skin_texture
+			skin_material.albedo_texture = skin_texture
 		for mesh_ins: MeshInstance3D in skin_mesh_list:
-			mesh_ins.set_surface_override_material(0, material)
+			mesh_ins.set_surface_override_material(0, skin_material)
+
+@export var skin_material: StandardMaterial3D
 
 @export_subgroup("Mesh")
 @export var skin_mesh_list: Array[MeshInstance3D]
